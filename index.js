e75f458c001f324b768a42165460b44a10be9d75
rmamoonx <r.mamoonx@hotmail.com>
2021-04-01 19:29:38 +0400

    Lint

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient;
-const app = express()
-const port = 3000
-const User = require('./models/User');
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
+const port = 3000;
+const User = require("./models/User");
 
 app.use(bodyParser.json());
 
-
-const uri = "mongodb+srv://fastech:<password>@fastech.okn4v.mongodb.net/Fastech?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    console.log("Database is connected");
+const uri =
+  "mongodb+srv://fastech:<password>@fastech.okn4v.mongodb.net/Fastech?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  console.log("Database is connected");
   client.close();
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
 
 app.post("/signup", async (req, res) => {
-    var newUser = new User({
-      firstName: req.body.fname,
-      lasttName: req.body.lname,
-      email: req.body.email,
-      password: req.body.password,
-      role : req.body.role
-    });
-    await newUser
-      .save()
-      .then(() => {
-        res.status(200).send(newUser);
-      })
-      .catch(err => {
-        console.log("Error is ", err.message);
-      });
+  var newUser = new User({
+    firstName: req.body.fname,
+    lasttName: req.body.lname,
+    email: req.body.email,
+    password: req.body.password,
+    role: req.body.role,
   });
-
-
-
+  await newUser
+    .save()
+    .then(() => {
+      res.status(200).send(newUser);
+    })
+    .catch((err) => {
+      console.log("Error is ", err.message);
+    });
+});
 
 app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`)
-})
+  console.log(`app listening at http://localhost:${port}`);
+});
--- models/User.js
+++ models/User.js
@@ -1,25 +1,24 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
-   firstName:{
-     type:String,
-     require:true
-   },
-   lastName:{
-    type:String,
-    require:true
+  firstName: {
+    type: String,
+    require: true,
   },
-  email:{
-    type:String,
-    require:true
+  lastName: {
+    type: String,
+    require: true,
   },
-   password:{
-     type:String,
-     require:true
-   },
-   role:{
-       type:Boolean,
-       require:true
-   }
-
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  role: {
+    type: Boolean,
+    require: true,
+  },
 });
-module.exports = User = mongoose.model('UserSchema',UserSchema);
+module.exports = User = mongoose.model("UserSchema", UserSchema);
