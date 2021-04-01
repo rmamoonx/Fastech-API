6f1b7a6d58a88cd4a2e5b41c84a022e89da3311f
Mamoon Raja <59180384+rmamoonx@users.noreply.github.com>
2021-04-01 17:32:21 +0400

    Added Post Method for Signup

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express()
 const port = 3000
 const User = require('./models/User');
-var db = require("./db/db").myurl;
 
 app.use(bodyParser.json());
 
@@ -12,14 +11,34 @@ app.use(bodyParser.json());
 const uri = "mongodb+srv://fastech:<password>@fastech.okn4v.mongodb.net/Fastech?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+    console.log("Database is connected");
   client.close();
 });
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
 })
+
+app.post("/signup", async (req, res) => {
+    var newUser = new User({
+      firstName: req.body.fname,
+      lasttName: req.body.lname,
+      email: req.body.email,
+      password: req.body.password,
+      role : req.body.role
+    });
+    await newUser
+      .save()
+      .then(() => {
+        res.status(200).send(newUser);
+      })
+      .catch(err => {
+        console.log("Error is ", err.message);
+      });
+  });
+
+
+
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
